4f6b40c007cf5871be28e237b992ef76763d88fc
chadams <chadams@student.wethinkcode.co.za>
2019-07-01 14:15:59 +0200

    started putting uml into code

---
+++ avaj-launcher/src/aircraft/JetPlane.java
@@ -0,0 +1,42 @@
+package aircraft;
+
+import Weather.Coordinates;
+import Weather.Flyable;
+import Weather.WeatherTower;
+
+public class JetPlane extends Aircrafts implements Flyable{
+
+	WeatherTower weatherTower;
+	protected JetPlane(String name, Coordinates coordinates) {
+		super(name, coordinates);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void updateConditions() {
+		// TODO Auto-generated method stub
+		String weahter = weatherTower.getWeather(_coordinates);
+		switch(weahter) {
+		case "RAIN" :
+			_coordinates = new Coordinates(_coordinates.getht() + 0, _coordinates.getlat() + 0, _coordinates.getlo() + 5);
+			break;
+		case "FOG" :
+			_coordinates = new Coordinates(_coordinates.getht() + 0, _coordinates.getlat() + 0, _coordinates.getlo() + 1);
+			break;
+		case "SUN" : 
+			_coordinates = new Coordinates(_coordinates.getht() + 2, _coordinates.getlat() + 0, _coordinates.getlo() + 10);
+			break;
+		case "SNOW" :
+			_coordinates = new Coordinates(_coordinates.getht() - 7, _coordinates.getlat() + 0, _coordinates.getlo() + 0);
+		}
+	}
+
+	@Override
+	public void registerTower(WeatherTower WeatherTower) {
+		// TODO Auto-generated method stub
+		weatherTower = WeatherTower;
+		WeatherTower.register(this);
+		
+	}
+
+}
---
+++ za.wethinkcode.avaj/simulate/vehicles/Baloon.java
@@ -0,0 +1,19 @@
+package simulate.vehicles;
+
+import vehicles.Flyable;
+
+public class Baloon extends Aircraft implements Flyable {
+    private WeatherTower    weatherTower;
+
+    private Baloon(String name, Coordinates coordinates) {
+
+    }
+
+    public void updateConditions() {
+
+    }
+
+    public void registerTower(WeatherTower weatherTower) {
+        
+    }
+}
