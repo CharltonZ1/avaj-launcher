febf65fc941c4221d196b2aa0c9cc28ca4d2e9cc
chadams <chadams@student.wethinkcode.co.za>
2019-07-02 14:06:08 +0200

    updated Tower and WeatherTower classes

--- za.wethinkcode.avaj/simulate/Tower.java
+++ za.wethinkcode.avaj/simulate/Tower.java
@@ -4,15 +4,19 @@ import java.util.List;
 public class Tower {
     private List<Flyable> observers = new ArrayList<Flyable>();
 
-    public void register(Flyable flyable) {
+    public void     register(Flyable flyable) {
         if (observers.contains(flyable))
             return ;
         observers.add(flyable);
     }
 
-    public void unregister(Flyable flyable) {
+    public void     unregister(Flyable flyable) {
         if (observers.contains(flyable))
             observers.remove(flyable);
         return ;
+    }
+
+    protected void  conditionsChanged() {
+        
     }
 }
---
+++ za.wethinkcode.avaj/simulate/WeatherTower.java
@@ -0,0 +1,7 @@
+import simulate.vehicles.Coordinates;
+
+public class WeatherTower extends Tower {
+    public String   getWeather(Coordinates coordinates) {
+        return (WeatherProvider.getProvider().getCurrentWeather(coordinates));
+    }
+}
