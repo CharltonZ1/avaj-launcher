2bf0e62fde6cf754e754d918deac678cec274990
Charlton ADAMS <chadams@c3r1s2.wethinkcode.co.za>
2019-07-09 09:32:11 +0200

    removed clutter from repo cache

--- za.wethinkcode.avaj/Flyable.java
+++
@@ -1,9 +0,0 @@
-package za.wethinkcode.avaj;
-
-import za.wethinkcode.avaj.*;
-
-public interface Flyable {
-    public void updateConditions();
-
-    public void registerTower(WeatherTower WeatherTower);
-}
